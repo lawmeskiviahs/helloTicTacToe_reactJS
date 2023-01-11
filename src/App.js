4f205b3cbd687e4de152ec69f3d63c8445e94291
Shaivik Semwal <shaiviksemwal@gmail.com>
2023-01-11 11:36:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  let name = 'whatever'
+  return (
+    <div className="App">
+      React
+      {name}
+    </div>
+  );
+}
+
+export default App;
+// export default function Square() {
+//   return <button className="square">X</button>;
+// }
