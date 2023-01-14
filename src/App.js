23392e3531317f8cf5d2ca0c080388fc42a26d99
Shaivik Semwal <shaiviksemwal@gmail.com>
2023-01-14 13:15:11 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-function App() {
-  let name = 'whatever'
+import { useState } from "react";
+
+function Square({ value }) {
+  const [testValue, setTestValue] = useState("Y");
+  function handleClick() {
+    console.log(value, " clicked");
+  setTestValue("Z");
+  }
   return (
-    <div className="App">
-      React
-      {name}
-    </div>
+    // <button className="square" onClick={handleClick}>
+    <button className="square" onClick={handleClick}>{value}{testValue}</button>
   );
 }
 
-export default App;
-// export default function Square() {
-//   return <button className="square">X</button>;
-// }
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill('X'));
+  return (
+    <>
+      <div className="board-row">
+        {/* <Square value="1" /> */}
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
+
+export default Board;
